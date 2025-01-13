7843018b44c1db10f91415d4ca314de8ef76b42f
CodeOmari <jacksonomari2230@gmail.com>
2025-01-13 11:26:17 +0300

    Updated URLs

--- NovaCare/urls.py
+++ NovaCare/urls.py
@@ -22,6 +22,6 @@ from main_app import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
+    path('careers/', views.careers, name='careers'),
     path('admin/', admin.site.urls),
 ]
