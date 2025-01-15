965fed2a8547e66fb5169bfa4890e40a48c0c439
CodeOmari <jacksonomari2230@gmail.com>
2025-01-15 13:28:31 +0300

    Updated project models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class AdultPatient(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        db_table = 'adult patients'
+
+
+class ChildPatient(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=10)
+    parent_first_name = models.CharField(max_length=30)
+    parent_last_name = models.CharField(max_length=30)
+    parent_gender = models.CharField(max_length=10)
+    parent_phone_number = models.CharField(max_length=10, unique=True)
+    relationship = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        db_table = 'child patients'
