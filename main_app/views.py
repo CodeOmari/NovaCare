e8524fbf3d22c9a0af4b3bab26a47842c1ba3bfd
CodeOmari <jacksonomari2230@gmail.com>
2025-01-15 13:29:26 +0300

    updated views file

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from main_app.app_forms import ChildForm, AdultForm
+from main_app.models import AdultPatient, ChildPatient
+
 
 # Create your views here.
 def home(request):
@@ -9,3 +15,26 @@ def about(request):
 
 def careers(request):
     return render(request, 'Careers.html')
+
+def add_client(request):
+    if request.method == 'POST':
+        form = ChildForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration completed successfully!')
+            return redirect('home')
+    else:
+        form = ChildForm()
+    return render(request, 'children_form.html', {'form': form})
+
+
+def add_adult_client(request):
+    if request.method == 'POST':
+        form = AdultForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration completed successfully!')
+            return redirect('home')
+    else:
+        form = AdultForm()
+    return render(request, 'adult_form.html', {'form': form})
