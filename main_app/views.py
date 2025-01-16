7e9c3d7b3fb6ab610534bfee695d0b21369f7a83
CodeOmari <jacksonomari2230@gmail.com>
2025-01-16 21:24:21 +0300

    Updated views file

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,30 @@
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from main_app.app_forms import ChildForm, AdultForm
+from main_app.app_forms import ChildForm, AdultForm, LoginForm
 from main_app.models import AdultPatient, ChildPatient
 
 
 # Create your views here.
+@login_required
 def home(request):
     return render(request, 'Home.html')
 
+@login_required
 def about(request):
     return render(request, 'About.html')
 
+
+@login_required
 def careers(request):
     return render(request, 'Careers.html')
 
+
+@login_required
 def add_client(request):
     if request.method == 'POST':
         form = ChildForm(request.POST)
@@ -28,6 +37,7 @@ def add_client(request):
     return render(request, 'children_form.html', {'form': form})
 
 
+@login_required
 def add_adult_client(request):
     if request.method == 'POST':
         form = AdultForm(request.POST)
@@ -38,3 +48,39 @@ def add_adult_client(request):
     else:
         form = AdultForm()
     return render(request, 'adult_form.html', {'form': form})
+
+
+def login_user(request):
+    if request.method == "GET":
+        form = LoginForm()
+        return render(request, "login_form.html", {"form": form})
+    elif request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
+        messages.error(request, "Invalid username or password")
+        return render(request, "login_form.html", {"form": form})
+
+
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('login')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account creation for {username} was successful!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'Register.html', {'form': form})
