55fb76631d0606f43f4e79f86ac46488d6be9bf9
CodeOmari <jacksonomari2230@gmail.com>
2025-01-16 21:21:27 +0300

    Added django admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from main_app.models import AdultPatient, ChildPatient
+
 # Register your models here.
+
+admin.site.register(AdultPatient)
+
+admin.site.register(ChildPatient)
+
+admin.site.site_header = 'NovaCare Administration'
+
+admin.site.site_title = 'NovaCare Administration'
+
+#NCadmin@gmail.com
