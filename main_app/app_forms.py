b50fcd4756ba3a89278c873503164e1863998c3f
CodeOmari <jacksonomari2230@gmail.com>
2025-01-20 11:49:24 +0300

    updated forms file

--- main_app/app_forms.py
+++ main_app/app_forms.py
@@ -1,6 +1,8 @@
+import datetime
+
 from django import forms
 
-from main_app.models import ChildPatient, AdultPatient
+from main_app.models import ChildPatient, AdultPatient, AdultAppointments
 
 GENDER_CHOICES = {"Male": "Male", "Female": "Female"}
 RELATIONSHIP_CHOICES = {"Father": "Father", "Mother": "Mother", "Sibling": "Sibling", "Relative": "Relative",
@@ -22,6 +24,43 @@ class AdultForm(forms.ModelForm):
     class Meta:
         model = AdultPatient
         fields = ['first_name', 'last_name', 'gender','phone_number']
+
+
+DEPARTMENT_CHOICES = {
+    " ": " ",
+    "Emergency": "Emergency",
+    "Counselling": "Counselling",
+    "Radiology": "Radiology",
+    "Physiotherapy": "Physiotherapy",
+    "Antenatal": "Antenatal",
+    "Orthopedics": "Orthopedics",
+    "Gynaecology": "Gynaecology",
+    "Oncology": "Oncology",
+    "Pathology": "Pathology"
+}
+APPOINTMENT_CHOICES = {
+    ' ': ' ',
+    'Consultation': 'Consultation',
+    'Follow-up treatment': 'Follow-up treatment',
+    'Diagnostic test': 'Diagnostic test'
+}
+TIME_CHOICES = {
+    ' ': ' ',
+    '9:00AM - 10:30AM': '9:00AM - 10:30AM',
+    '11:00AM - 12:30PM': '11:00AM - 12:30PM',
+    '2:00PM - 03:30PM': '2:00PM - 03:30PM'
+}
+class AdultAppointment(forms.ModelForm):
+    department = forms.ChoiceField(choices=DEPARTMENT_CHOICES, widget=forms.Select)
+    appointment_type = forms.ChoiceField(choices=APPOINTMENT_CHOICES, widget=forms.Select)
+    appointment_time = forms.ChoiceField(choices=TIME_CHOICES, widget=forms.Select)
+    class Meta:
+        model = AdultAppointments
+        fields = ['age', 'appointment_date', 'appointment_time',
+                  'appointment_type', 'department']
+        widgets = {
+            'appointment_date': forms.DateInput(attrs={'type': 'date', 'max': datetime.date.today() + datetime.timedelta(days=7)}),
+        }
 
 
 
