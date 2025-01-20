a5c564222c3c9edb7448381f7a79432ee3165cab
CodeOmari <jacksonomari2230@gmail.com>
2025-01-20 11:50:20 +0300

    updated project models

--- main_app/models.py
+++ main_app/models.py
@@ -34,3 +34,37 @@ class ChildPatient(models.Model):
 
     class Meta:
         db_table = 'child_patients'
+
+
+class AdultAppointments(models.Model):
+    patient = models.ForeignKey(AdultPatient, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    appointment_date = models.DateField()
+    appointment_time = models.CharField(max_length=50)
+    appointment_type = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.patient.first_name} {self.patient.last_name}'
+
+    class Meta:
+        db_table = 'adult_appointments'
+
+
+class ChildAppointments(models.Model):
+    patient = models.ForeignKey(ChildPatient, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    appointment_date = models.DateField()
+    appointment_time = models.CharField(max_length=50)
+    appointment_type = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.patient.first_name} {self.patient.last_name}'
+
+    class Meta:
+        db_table = 'child_appointments'
