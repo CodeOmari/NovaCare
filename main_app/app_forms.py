0c5df0dee95b66af1955258ec823fd6bf8c8ec44
CodeOmari <jacksonomari2230@gmail.com>
2025-02-03 08:46:37 +0300

    project forms

--- main_app/app_forms.py
+++ main_app/app_forms.py
@@ -2,28 +2,7 @@ import datetime
 
 from django import forms
 
-from main_app.models import ChildPatient, AdultPatient, AdultAppointments
-
-GENDER_CHOICES = {"Male": "Male", "Female": "Female"}
-RELATIONSHIP_CHOICES = {"Father": "Father", "Mother": "Mother", "Sibling": "Sibling", "Relative": "Relative",
-                        "Guardian": "Guardian"}
-class ChildForm(forms.ModelForm):
-    gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.RadioSelect)
-    parent_gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.RadioSelect)
-    relationship = forms.ChoiceField(choices=RELATIONSHIP_CHOICES, widget=forms.RadioSelect)
-    class Meta:
-        model = ChildPatient
-        fields = ['first_name', 'last_name', 'gender','parent_first_name',
-                  'parent_last_name', 'parent_gender', 'parent_phone_number',
-                  'relationship']
-
-
-ADULT_CHOICES = {"Male": "Male", "Female": "Female"}
-class AdultForm(forms.ModelForm):
-    gender = forms.ChoiceField(choices=ADULT_CHOICES, widget=forms.RadioSelect)
-    class Meta:
-        model = AdultPatient
-        fields = ['first_name', 'last_name', 'gender','phone_number']
+from main_app.models import Appointments
 
 
 DEPARTMENT_CHOICES = {
@@ -50,12 +29,12 @@ TIME_CHOICES = {
     '11:00AM - 12:30PM': '11:00AM - 12:30PM',
     '2:00PM - 03:30PM': '2:00PM - 03:30PM'
 }
-class AdultAppointment(forms.ModelForm):
+class Appointment(forms.ModelForm):
     department = forms.ChoiceField(choices=DEPARTMENT_CHOICES, widget=forms.Select)
     appointment_type = forms.ChoiceField(choices=APPOINTMENT_CHOICES, widget=forms.Select)
     appointment_time = forms.ChoiceField(choices=TIME_CHOICES, widget=forms.Select)
     class Meta:
-        model = AdultAppointments
+        model = Appointments
         fields = ['age', 'appointment_date', 'appointment_time',
                   'appointment_type', 'department']
         widgets = {
