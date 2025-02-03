2e393e784eb7e4966d5ae66c0e5bbc9949f90594
CodeOmari <jacksonomari2230@gmail.com>
2025-02-03 12:43:22 +0300

    added admin panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 
-from main_app.models import Appointments
+from main_app.models import Appointment, Patient
 
 # Register your models here.
 
-admin.site.register(Appointments)
+admin.site.register(Appointment)
+admin.site.register(Patient)
 
 admin.site.site_header = 'NovaCare Administration'
 
 admin.site.site_title = 'NovaCare Administration'
 
 # novacare@gmail.com
-# nova
+# novacare
