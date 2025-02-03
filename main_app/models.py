110ba967df2478635ecf295281569331c14986d2
CodeOmari <jacksonomari2230@gmail.com>
2025-02-03 12:45:18 +0300

    added patients model

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
 # Create your models here.
-
-class Appointments(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    age = models.IntegerField()
+class Patient(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     gender = models.CharField(max_length=6)
-    phone_number = models.IntegerField()
+    phone_number = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        db_table = "patients"
+
+
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     appointment_date = models.DateField()
     appointment_time = models.CharField(max_length=50)
     appointment_type = models.CharField(max_length=100)
@@ -16,8 +26,8 @@ class Appointments(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return f'{self.patient.first_name} {self.patient.last_name}'
 
     class Meta:
-        db_table = 'appointment'
+        db_table = 'appointments'
 
