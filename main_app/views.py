72b6659e1b10d10427d2c4d8520dff3a1bfbfc0d
CodeOmari <jacksonomari2230@gmail.com>
2025-02-03 12:45:53 +0300

    updated views file

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
-from main_app.app_forms import  LoginForm, Appointment
+from main_app.app_forms import LoginForm, AppointmentForm, RegistrationForm
+from main_app.models import Patient
 
 
 # Create your views here.
@@ -78,6 +79,39 @@ def maternity_services(request):
 
 @login_required
 def book_appointment(request):
-    return None
+    if request.method == "POST":
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Appointment booking was successful!')
+            return redirect('home')
+    else:
+        form = AppointmentForm()
+    return render(request, 'appointment_form.html', {'form': form})
+
+@login_required
+def appointments(request, patient_id):
+    client = get_object_or_404(Patient, id=patient_id)
+    if request.method == "POST":
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            appointment = form.save(commit=False)
+            appointment.patient = patient
+            appointment.save()
+            messages.success(request, 'Appointment booked successfully!')
+            return redirect('home')
+    else:
+        form = AppointmentForm()
+    return render(request, 'appointment_form.html', {'form': form, 'patient':patient})
 
 
+def patient(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration was successful!')
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register_form.html', {'form': form})
