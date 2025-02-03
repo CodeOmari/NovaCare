8a616c7daffd0e42172589d935596f3896441e92
CodeOmari <jacksonomari2230@gmail.com>
2025-02-03 08:47:41 +0300

    updated project models

--- main_app/models.py
+++ main_app/models.py
@@ -2,43 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class AdultPatient(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    gender = models.CharField(max_length=10)
-    phone_number = models.CharField(max_length=10, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.first_name + " " + self.last_name
-
-    class Meta:
-        db_table = 'adult_patients'
-
-
-class ChildPatient(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    gender = models.CharField(max_length=10)
-    parent_first_name = models.CharField(max_length=30)
-    parent_last_name = models.CharField(max_length=30)
-    parent_gender = models.CharField(max_length=10)
-    parent_phone_number = models.CharField(max_length=10)
-    relationship = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.first_name + " " + self.last_name
-
-    class Meta:
-        db_table = 'child_patients'
-
-
-class AdultAppointments(models.Model):
-    patient = models.ForeignKey(AdultPatient, on_delete=models.CASCADE)
+class Appointments(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     age = models.IntegerField()
+    gender = models.CharField(max_length=6)
+    phone_number = models.IntegerField()
     appointment_date = models.DateField()
     appointment_time = models.CharField(max_length=50)
     appointment_type = models.CharField(max_length=100)
@@ -47,24 +16,8 @@ class AdultAppointments(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.patient.first_name} {self.patient.last_name}'
+        return self.first_name + " " + self.last_name
 
     class Meta:
-        db_table = 'adult_appointments'
+        db_table = 'appointment'
 
-
-class ChildAppointments(models.Model):
-    patient = models.ForeignKey(ChildPatient, on_delete=models.CASCADE)
-    age = models.IntegerField()
-    appointment_date = models.DateField()
-    appointment_time = models.CharField(max_length=50)
-    appointment_type = models.CharField(max_length=100)
-    department = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'{self.patient.first_name} {self.patient.last_name}'
-
-    class Meta:
-        db_table = 'child_appointments'
