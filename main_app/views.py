4c77a716fc2ae71255f9fbc08ede9fde403f2aa6
CodeOmari <jacksonomari2230@gmail.com>
2025-02-03 08:48:21 +0300

    updated views files

--- main_app/views.py
+++ main_app/views.py
@@ -5,8 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
-from main_app.app_forms import ChildForm, AdultForm, LoginForm, AdultAppointment
-from main_app.models import AdultPatient, ChildPatient
+from main_app.app_forms import  LoginForm, Appointment
 
 
 # Create your views here.
@@ -21,32 +20,6 @@ def about(request):
 
 def careers(request):
     return render(request, 'Careers.html')
-
-
-@login_required
-def add_client(request):
-    if request.method == 'POST':
-        form = ChildForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Registration completed successfully!')
-            return redirect('home')
-    else:
-        form = ChildForm()
-    return render(request, 'children_form.html', {'form': form})
-
-
-@login_required
-def add_adult_client(request):
-    if request.method == 'POST':
-        form = AdultForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Registration completed successfully!')
-            return redirect('home')
-    else:
-        form = AdultForm()
-    return render(request, 'adult_form.html', {'form': form})
 
 
 def login_user(request):
@@ -84,36 +57,6 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'Register.html', {'form': form})
 
-
-@login_required
-def clients(request):
-    return render(request, 'client_details.html')
-
-
-def child_details(request):
-    return None
-
-
-def adult_details(request):
-    return None
-
-
-def update_adult_details(request):
-    return None
-
-
-def update_child_details(request):
-    return None
-
-
-def delete_child_details(request):
-    return None
-
-
-def delete_adult_details(request):
-    return None
-
-
 def emergency_services(request):
     return render(request, 'emergency.html')
 
@@ -134,19 +77,7 @@ def maternity_services(request):
     return render(request, 'maternity.html')
 
 @login_required
-def book_adult_appointment(request, id):
-    patient = get_object_or_404(AdultPatient, pk=id)
-
-    if request.method == 'POST':
-        form = AdultAppointment(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Appointment booked successfully!')
-            return redirect('home')
-    else:
-        form = AdultAppointment()
-    return render(request, 'adult_appointment_form.html', {'form': form})
-
-
-def book_child_appointment(request):
+def book_appointment(request):
     return None
+
+
