dcc03e9ea418ea8bcada73e94dba06304bbef356
CodeOmari <jacksonomari2230@gmail.com>
2025-06-21 14:22:37 +0300

    update project admin panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 
-from main_app.models import Appointment, Patient
-
 # Register your models here.
 
-admin.site.register(Appointment)
-admin.site.register(Patient)
 
 admin.site.site_header = 'NovaCare Administration'
-
 admin.site.site_title = 'NovaCare Administration'
 
 # novacare@gmail.com
