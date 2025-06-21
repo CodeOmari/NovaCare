2cc1226fa03a77604171e8e205b3929500786bd8
CodeOmari <jacksonomari2230@gmail.com>
2025-06-21 14:23:47 +0300

    update models file

--- main_app/models.py
+++ main_app/models.py
@@ -1,33 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Patient(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    gender = models.CharField(max_length=6)
-    phone_number = models.CharField(max_length=10)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-    class Meta:
-        db_table = "patients"
-
-
-class Appointment(models.Model):
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    appointment_date = models.DateField()
-    appointment_time = models.CharField(max_length=50)
-    appointment_type = models.CharField(max_length=100)
-    department = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'{self.patient.first_name} {self.patient.last_name}'
-
-    class Meta:
-        db_table = 'appointments'
-
