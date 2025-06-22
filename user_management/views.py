c795a807d5d50655eeb6a71754c52883c72d715a
CodeOmari <jacksonomari2230@gmail.com>
2025-06-22 14:38:17 +0300

    add user management views

---
+++ user_management/views.py
@@ -0,0 +1,35 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from user_management.app_forms import LoginForm
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Create your views here.
+def login_user(request):
+    if request.method == "GET":
+        form = LoginForm()
+        return render(request, "login.html", {"form": form})
+    elif request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('careers')
+        messages.error(request, "Invalid username or password")
+        return render(request, "login.html", {"form": form})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account creation for {username} was successful!')
+            return redirect('user_management:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
