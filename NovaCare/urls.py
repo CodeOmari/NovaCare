c0fce1a699d1d21b431947ab2c1a8ef862eb4435
CodeOmari <jacksonomari2230@gmail.com>
2025-06-22 14:37:19 +0300

    add user management urls

--- NovaCare/urls.py
+++ NovaCare/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from main_app import views
 
@@ -28,5 +28,9 @@ urlpatterns = [
     path('surgical/services/', views.surgical_services, name='surgical_services'),
     path('maternity/services/', views.maternity_services, name='maternity_services'),
     path('careers/', views.careers, name='careers'),
+
+
+    path('users/', include('user_management.urls')),
+
     path('admin/', admin.site.urls),
 ]
