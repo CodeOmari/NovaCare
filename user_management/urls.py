e3f93940b0771d974a5875f61250d6d4d7f2b824
CodeOmari <jacksonomari2230@gmail.com>
2025-06-23 10:35:50 +0300

    add user and staff urls

--- user_management/urls.py
+++ user_management/urls.py
@@ -4,6 +4,11 @@ from user_management import views
 app_name = 'user_management'
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.login_user, name='login'),
     path('register/', views.register, name='register'),
+
+
+    path('portal/', views.dashboard, name='dashboard'),
+    path('staff/', views.staff_dashboard, name='staff_dashboard'),
+    path('users/', views.patient_dashboard, name='patient_dashboard'),
 ]
