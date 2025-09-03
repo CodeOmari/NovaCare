704b73b23ad63ef720dc737e323fd180c1b8a9d8
CodeOmari <jacksonomari2230@gmail.com>
2025-09-03 12:12:51 +0300

    add password reset and password confirmation urls

--- user_management/urls.py
+++ user_management/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('logout/', views.signout_user, name='logout'),
 
+    path('password-reset/', views.password_reset_request, name='password_reset_request'),
+    path('reset-password-confirm/<str:token>/', views.password_reset_confirm, name='password_reset_confirm'),
+
 
     path('portal/', views.dashboard, name='dashboard'),
     path('staff/', views.staff_dashboard, name='staff_dashboard'),
