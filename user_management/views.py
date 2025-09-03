4aafcd5e85a8adcf8695b486ea503f602afde2fa
CodeOmari <jacksonomari2230@gmail.com>
2025-09-03 12:13:45 +0300

    add logic to handle password reset request and password reset confirmation

--- user_management/views.py
+++ user_management/views.py
@@ -2,8 +2,17 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from user_management.app_forms import LoginForm
+from user_management.app_forms import LoginForm, PasswordResetRequestForm, SetNewPasswordForm
 from django.contrib.auth.forms import UserCreationForm
+
+from django.core.signing import TimestampSigner, SignatureExpired, BadSignature
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.core.mail import send_mail
+from django.contrib.auth.hashers import make_password
+
+
+signer = TimestampSigner() # creates signed tokens
 
 
 # Create your views here.
@@ -39,6 +48,53 @@ def signout_user(request):
     logout(request)
     return redirect('home')
 
+# Request reset via email
+def password_reset_request(request):
+    if request.method == "POST":
+        form = PasswordResetRequestForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            try:
+                user = User.objects.get(email=email)
+                token = signer.sign(user.pk) # sign user id
+                reset_link = request.build_absolute_uri(f"/reset-password-confirm/{token}")
+
+                # send email
+                send_mail("Password Reset Request",
+                          f"Click the link below to reset your password:\n{reset_link}",
+                          settings.DEFAULT_FROM_EMAIL,
+                          [email],
+                          fail_silently=False,
+                )
+                messages.success(request, f'Your password reset link has been sent to {email}.')
+                return redirect('user_management:login')
+            except User.DoesNotExist:
+                messages.error(request, f'No user found with email {email}.')
+    else:
+        form = PasswordResetRequestForm()
+    return render(request, "password_reset_request.html", {'form': form})
+
+
+# Reset password using token
+def password_reset_confirm(request, token):
+    try:
+        user_id = signer.unsign(token, max_age=3600) # token id valid for 1 hour
+        user = User.objects.get(pk=user_id)
+    except (BadSignature, SignatureExpired, User.DoesNotExist):
+        messages.error(request, 'Invalid or expired reset link.')
+        return redirect("user_management:password_reset_request")
+
+    if request.method == "POST":
+        form = SetNewPasswordForm(request.POST)
+        if form.is_valid():
+            user_password = form.cleaned_data["new_password"]
+            user.password = make_password(user_password)
+            user.save()
+            messages.success(request, 'Password updated successfully!')
+            return redirect('user_management:login')
+    else:
+        form = SetNewPasswordForm()
+    return render(request, 'password_reset_confirm.html', {'form': form})
 
 
 
