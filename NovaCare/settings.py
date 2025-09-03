0c1fbc2d292e2824f69f3d01ef20406031fe238c
CodeOmari <jacksonomari2230@gmail.com>
2025-09-03 12:12:17 +0300

    add email configuration settings for password resetting

--- NovaCare/settings.py
+++ NovaCare/settings.py
@@ -151,5 +151,13 @@ CSRF_TRUSTED_ORIGINS = [
 # disable secure cookies
 CSRF_COOKIE_SECURE = False
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "your_email@gmail.com"
+EMAIL_HOST_PASSWORD = "your_app_password"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 LOGIN_URL = 'login'
