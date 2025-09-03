0254dba0f7a6f61bce340c0b31131b52bde7202e
CodeOmari <jacksonomari2230@gmail.com>
2025-09-03 12:08:03 +0300

    add password reset and set new password forms

--- user_management/app_forms.py
+++ user_management/app_forms.py
@@ -3,3 +3,23 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class PasswordResetRequestForm(forms.Form):
+    email = forms.EmailField(label='Enter your email address')
+
+
+class SetNewPasswordForm(forms.Form):
+    new_password = forms.CharField(label='New Password',
+                                   widget=forms.PasswordInput(attrs={'placeholder': 'Enter new password'}))
+    confirm_password = forms.CharField(label='Confirm Password',
+                                       widget=forms.PasswordInput(attrs={'placeholder': 'Confirm new password'}))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("new_password")
+        password2 = cleaned_data.get("confirm_password")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return cleaned_data
