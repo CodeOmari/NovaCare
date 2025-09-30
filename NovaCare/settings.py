fe57ab87e5112eee85e1020941ef120182b8d4e3
CodeOmari <jacksonomari2230@gmail.com>
2025-09-30 06:05:04 +0300

    remove pycharm configuration files from version control

--- NovaCare/settings.py
+++ NovaCare/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'NovaCare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
