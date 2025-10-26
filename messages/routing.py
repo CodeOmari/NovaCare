03327fd45288ac3e3ef24409aa8a3d642f52193b
CodeOmari <jacksonomari2230@gmail.com>
2025-10-27 01:17:07 +0300

    update app name to messaging

--- messages/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-# Match WebSocket URLs to specific consumers
-websocket_urlpatterns = [
-    re_path(r'ws/message/(?P<conversation_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
-]
--- messages/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Message
-
-@receiver(post_save, sender=Message)
-def notify_message(sender, instance, created, **kwargs):
-    if created:
-        # Here you could send push, email, or in-app notifications
-        print(f"📩 New message from {instance.sender.username} in conversation {instance.conversation.id}")
