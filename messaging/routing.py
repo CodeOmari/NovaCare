4084e57f68c9577f38894191d812734da52fdadf
CodeOmari <jacksonomari2230@gmail.com>
2025-12-12 08:46:11 +0300

    remove messaging app routing file

--- messaging/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-# Match WebSocket URLs to specific consumers
-# websocket_urlpatterns is imported by the asgi.py file to set up the WebSocket Router
-websocket_urlpatterns = [
-    re_path(r'ws/message/(?P<conversation_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
-]
